40990cb72ad5080c3833b5922dc37ee30438e3fe
古藤勇魚 <>
2020-01-30 11:29:17 +0800

    テキスト入力

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,14 @@
 $(window).on('load',function(){
     $('.loader').slideUp(1000);
 })
-$('.container2').ripples({
+$('body').ripples({
     resolution: 400
 });
 
+// $('.fish4').scroll(function(){
+//     $()
+// })
+
+$('#how').on('click',function(){
+    $(this).siblings().slideToggle();
+});
