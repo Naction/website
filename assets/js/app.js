64afcb198d58718f3059a000ce7fce0fff783437
古藤勇魚 <>
2020-01-30 21:44:36 +0800

    70% done

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,7 @@ $('body').ripples({
     resolution: 400
 });
 
+
 // $('.fish4').scroll(function(){
 //     $()
 // })
@@ -12,3 +13,15 @@ $('body').ripples({
 $('#how').on('click',function(){
     $(this).siblings().slideToggle();
 });
+
+
+$(window).scroll(function (){
+    $('.fadein').each(function(){
+        var elemPos = $(this).offset().top,
+            scroll = $(window).scrollTop(),
+            windowHeight = $(window).height();
+          if (scroll > elemPos - windowHeight + 100){
+              $(this).addClass('scrollin');
+            }
+        });
+    });
